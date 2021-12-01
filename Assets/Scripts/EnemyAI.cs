882813804cda5bd7f4d865bd01a1d2e55a848619
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-12-02 01:31:58 +0330

    Make Sandbox and Enemy AI to chase the player.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;// We need the position of the player so we use "Transform"
+    NavMeshAgent navMeshAgent; // Select the "NavMeshAgent" and click "ctrl + ." to suggest which library you need to add
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);// This way, this game object will set it's destination to the target and follows it
+    }
+}
