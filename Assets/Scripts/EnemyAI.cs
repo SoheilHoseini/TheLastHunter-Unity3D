4f018a04b287bf561eecdf9847b2120100c4aadd
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-12-15 13:30:41 +0330

    Set a chase range for enemy and visualize it

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,17 +6,35 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;// We need the position of the player so we use "Transform"
+    [SerializeField] float chaseRange = 200f;// Set the chase range of the enemy that player should not get closer than that
+
     NavMeshAgent navMeshAgent; // Select the "NavMeshAgent" and click "ctrl + ." to suggest which library you need to add
 
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;// At the begginging, the zombie should not think target is close enough
+
+    
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);// This way, this game object will set it's destination to the target and follows it
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        
+        if (distanceToTarget <= chaseRange)
+        {
+
+            navMeshAgent.SetDestination(target.position);// This way, this game object will set it's destination to the target and follows it
+        }
     }
+
+    void OnDrawGizmosSelected()
+    {
+        //Display chase range of enemy when we select it
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+    
 }
