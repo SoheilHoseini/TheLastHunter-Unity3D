564f8e12e6c97df58cfcce074d6168190282f5fb
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-12-29 21:36:57 +0330

    Enemy health system implemented.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     //we use this to set start position of firing and the diresction
     [SerializeField] Camera FPCamera;
     [SerializeField] float rangeOfShot = 100f;
+    [SerializeField] float damageAmount = 20f;
 
     void Update()
     {
@@ -21,9 +22,20 @@ public class Weapon : MonoBehaviour
     {
         //Calculate the fire info
         RaycastHit hit;//information of our ray, colliding with any object
-        bool isHit = Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward , out hit, rangeOfShot);
+        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward , out hit, rangeOfShot))
+        {
+            Debug.Log("You hit " + hit.transform.name);
+            //To Do: add some hit effects for visual players
 
-        Debug.Log("You hit " + hit.transform.name);
-        
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            //call a method on EnemyHealth that decreases enemy health
+            if (target == null) return; //This way if we hit sth other than enemy, will not get NullReference error
+            target.TakeDamage(damageAmount);
+        }      
+        else
+        {
+            //To avoid NullReference when shooting the sky
+            return;
+        }
     }
 }
