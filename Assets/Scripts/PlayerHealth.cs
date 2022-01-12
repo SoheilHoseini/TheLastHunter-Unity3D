3804b3b5a696e653f48c39c04bf392a5a88fed5e
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-01-12 22:55:12 +0330

    Player health class made.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float playerHitPoint = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        playerHitPoint -= damage;
+        if(playerHitPoint <= 0)
+        {
+            Debug.Log("You are getting shot baby!");
+        }
+    }
+}
