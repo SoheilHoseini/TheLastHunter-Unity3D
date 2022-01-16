f7dcf767e56a49cd8521ef39816138224d475919
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-01-16 21:38:34 +0330

    Game over user interface.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    //reload the game when we hit "Play Again" button
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //reload the game when we hit "Quit" button
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
