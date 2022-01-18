e8875589f942437228bb28d4e0596aea361a38ed
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-01-18 20:24:43 +0330

    Weapon zoom implemented

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;// fps: First Person 
+    [SerializeField] float zoomedOutFOV = 60f; //FOV: Field of View
+    [SerializeField] float zoomedInFOV = 20f;
+
+    bool zoomedInToggle = false;
+
+    private void Update()
+    {
+        //If the right the mouse button is clicked
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomedInFOV;
+            }
+
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+}
