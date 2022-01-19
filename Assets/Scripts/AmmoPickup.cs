2c60cbb5a9dfb2c9ee4a650ef6729ee9fc237f0d
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-01-19 12:43:07 +0330

    Basic ammo implemented.

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    //when the player collides with the ammo, he takes it
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("You picked up ammo!");
+            Destroy(gameObject);// destroy the ammo when it is picked
+        }
+    }
+}
