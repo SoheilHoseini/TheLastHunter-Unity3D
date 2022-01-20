3f1c10079ff302e303a5fd1e4bf87e160784bb10
Soheil Hoseini <soheil.hosseini78@gmail.com>
2022-01-20 19:10:28 +0330

    Flash light with power fading.

---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;// How much the flash light is fading over time
+    [SerializeField] float angleDecay = 1f; // The amount of range decreasing
+    [SerializeField] float minimumAngle = 40f; // Flash light angle can not go below this
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+
+    private void DecreaseLightAngle()
+    {
+        //flash light angle should not go below minimumAngle
+        if (myLight.spotAngle <= minimumAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    
+}
